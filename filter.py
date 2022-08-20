3932085644251d84642e0dce2799a4d4f6b4b08b
rikalee0121 <93050785+rikalee0121@users.noreply.github.com>
2022-08-20 16:05:13 +0800

    add filtering function.py

---
+++ filter.py
@@ -0,0 +1,25 @@
+data = []
+count = 0
+with open ('reviews.txt','r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+print('檔案讀取完了，總共有',len(data),'筆資料')
+
+sum_len = 0
+for d in data: #每一個d當作字串
+	sum_len = sum_len + len(d)
+print('留言的平均長度為',sum_len/len(data))
+
+new = []
+for d in data:
+#d:字串; data:清單
+#一百萬筆留言當中的每一筆留言都叫做d
+#而data是裝著100萬筆的清單
+	if len(d) < 100:
+		new.append(d)
+print('一共有',len(new),'筆留言長度小於100')
+print(new[0])
+print(new[1])
